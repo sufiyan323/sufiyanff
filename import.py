3cfad9f3b0c0528cbe56b47efddeeeac367023b5
sufiyan323 <137264202+sufiyan323@users.noreply.github.com>
2023-06-21 14:42:23 +0530

    Add files via upload

---
+++ import.py
@@ -0,0 +1,22 @@
+
+def summation(a,b):
+    return a+b;
+def subtraction(a,b):
+    return a-b;
+def multiplication(a,b):
+    return a*b; 
+def divide(a,b):
+    return a/b;
+
+from cal import summation ,divide
+a = int(input("Enter the first number"))
+b = int(input("Enter the second number"))
+print("Sum = ",summation(a,b))
+print("Division = ",divide(a,b))
+
+from cal*
+a = int(input("Enter the first number"))
+b = int(input("Enter the second number"))
+print("Sum = ",summation(a,b))
+print("Product = ",multiplication(a,b))
+print("Difference = ",subtract(a,b))
